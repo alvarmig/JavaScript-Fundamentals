8d09ebdeec937e4b570c1f6ff9c506cb4569d2b1
alvarmig <43011534+alvarmig@users.noreply.github.com>
2019-04-09 19:06:23 -0500

    course end

--- angularapp/hangman/src/app/app.component.ts
+++ angularapp/hangman/src/app/app.component.ts
@@ -7,11 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  word = 'AGUACATE';
+  hiddenWord = '';
+
+  tries = 0;
+  win = false;
+  lose = false;
+
   letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
   'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
   'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
   constructor() {
-    console.log('Se acaba de crear el App Component');
+    
+    this.hiddenWord = '_ '.repeat( this.word.length );
+  }
+
+  verify( letter ) {
+
+    this.charExists( letter );
+    const hiddenWordsArr = this.hiddenWord.split(' ');
+
+    for( var i = 0; i < this.word.length; i++ ) {
+      if (this.word[i] === letter) {
+        hiddenWordsArr[i] = letter;
+      }
+    }
+    this.hiddenWord = hiddenWordsArr.join(' ');
+    this.verifyWin();
+  }
+
+  verifyWin() {
+    const wordArr = this.hiddenWord.split(' ');
+    const evaluateWord = wordArr.join('');
+
+    if( evaluateWord === this.word ) {
+      this.win = true;
+      console.log('User WON');
+    }
+ 
+    if( this.tries >= 9 ) {
+      this.lose = true;
+      console.log('User LOOSE');
+    }
+  }
+  charExists( letter ) {
+    if( this.word.indexOf( letter ) >= 0 ) {
+    } else {
+      this.tries++;
+    }
   }
 }
