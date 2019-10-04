c28d132172febcff561425f0c681366f2541c52e
alvarmig <43011534+alvarmig@users.noreply.github.com>
2019-10-04 15:33:42 -0500

    to-do list exercise with javascript only & local storage

--- JavaScript Crash Course/JavaScript Wizeline/to-do-app/script.js
+++ JavaScript Crash Course/JavaScript Wizeline/to-do-app/script.js
@@ -5,12 +5,10 @@ const alert = document.querySelector('.alertMessage');
 let isComplete = false;
 
 let todos = [
-  { name: 'Planchar la ropa', isComplete: false },
-  { name: ' Hacer el Super', isComplete: false },
-  { name: ' Hacer la tarea', isComplete: false }
+  /* { name: 'Planchar la ropa', isComplete: false },
+  { name: ' Hacer el Super', isComplete: true },
+  { name: ' Hacer la tarea', isComplete: false }*/
 ];
-
-let tasks = [];
 
 addButton.addEventListener('click', addItemToList);
 
@@ -28,6 +26,7 @@ function addItemToList() {
 
   if (!isDuplicated) {
     todos.push({
+      id: todos.length,
       name: fieldText.value,
       isComplete: false
     });
@@ -44,18 +43,18 @@ function addItemToList() {
   }
 }
 
-function removeTask(taskName){
-    todos = todos.filter(task => {
-        return task.name !== taskName;
-    });
-    localStorage.setItem('tareas', JSON.stringify(todos));
-    loadTasks();
+function removeTask(taskId) {
+  todos = todos.filter(task => {
+    return task.id !== taskId;
+  });
+  localStorage.setItem('tareas', JSON.stringify(todos));
+  loadTasks();
 }
 
-function toggleComplete(taskName) {
-  const task = todos.find(task => task.name === taskName);
-  task.map(task => {
-    if (task.name === taskName) {
+function toggleComplete(taskId) {
+
+  todos.map(task => {
+    if (task.id == taskId) {
       task.isComplete = !task.isComplete;
     }
   });
@@ -79,14 +78,18 @@ function loadTasks() {
                     <div class="row">
                         <div class="col-xs-1 checkbox">
                             <label>
-                                <input type="checkbox" onchange="toggleComplete('${element.name}')">
+                                <input type="checkbox" onchange="toggleComplete('${
+                                  element.id
+                                }')" ${element.isComplete ? 'checked' : ''}>
                             </label>
                         </div>
-                        <div id="taskText" class="col-xs-9 task-text ${todos.isComplete} ? 'complete' : ''   ">
+                        <div id="taskText" class="col-xs-9 task-text ${
+                          element.isComplete ? 'complete' : ''
+                        }">
                             ${element.name}
                         </div>
                         <div class="col-xs-2 delete-icon-area">
-                            <button onclick="removeTask('${element.name}')">
+                            <button onclick="removeTask('${element.id}')">
                                 <i class="delete-icon glyphicon glyphicon-trash"></i>
                             </button>
                         </div>
@@ -101,14 +104,43 @@ function initializeTask() {
 
   const savedTasks = JSON.parse(text);
 
-  if(!savedTasks){
-      return
+  if (!savedTasks) {
+    return;
   }
 
-  for (let i = 0; i < savedTasks.length; i++) {
-    todos.push(savedTasks[i]);
-  }
+  /*if(todos.length > 0){
+    todos = todos.splice(0, todos.length);
+  }*/
+  todos.length > 0 ? (todos = todos.splice(0, todos.length)) : 
+
+  savedTasks.forEach((task, index) => {
+    
+        task.id = index;
+        todos.push(task);
+      });
 }
 
 initializeTask();
 loadTasks();
+
+function removeAllTasks() {
+  todos.splice(0, todos.length);
+  localStorage.setItem('tareas', JSON.stringify(todos));
+  loadTasks();
+}
+
+function removeCompletedTasks() {
+  todos = todos.filter(todo => !todo.isComplete);
+
+  localStorage.setItem('tareas', JSON.stringify(todos));
+  loadTasks();
+}
+
+function completeAllTasks() {
+
+  todos.forEach(task => {
+    task.isComplete = true;
+  });
+
+  loadTasks();
+}
